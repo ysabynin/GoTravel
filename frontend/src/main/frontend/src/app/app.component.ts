267406d05f00ae620556247a48f753041abea081
ysabynin <evgensabyka@mail.ru>
2017-01-19 18:11:57 +0300

    Completed tutorial by Angular2 "Tour of Heroes"

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,31 +1,11 @@
-import { Component } from '@angular/core';
-import {Hero} from "./hero";
-import {HeroService} from "./hero.service";
-import { OnInit } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Tour of Heroes1';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService){
-  };
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void{
-    this.getHeroes();
-  }
+export class AppComponent{
+  title = 'Tour of Heroes';
 }
--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -1,23 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import './rxjs-extensions';
 
-import { AppComponent } from './app.component';
+
+import {AppRoutingModule} from "./app-routing.module";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import {AppComponent} from "./app.component";
+import {HeroesComponent} from "./heroes.component";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroService} from "./hero.service";
+import {DashboardComponent} from "./dashboard.component";
+import {HeroSearchComponent} from "./hero-search.component";
+import {HeroSearchService} from "./hero-search.service";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService, HeroSearchService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
